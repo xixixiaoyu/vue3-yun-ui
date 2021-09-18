caf40962226ad5839e167f2910f3400eb54c0d88
张明华 <1416900346@qq.com>
2021-09-19 01:04:16 +0800

    Button样式非props属性的继承

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-  Button 示例
-  </div>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <Button @click="handleClick" size="small" @mouseEnter="handleClick">我是btn</Button>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: { Button },
+  methods: {
+    handleClick() {
+      console.log("handleClick");
+      
+    }
+  }
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :size="size">
+    <button>
+      <!-- v-bind="$attrs"绑定所有属性 -->
+      <slot v-bind="rest" />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, { attrs }) {
+    const { size, ...rest } = attrs;
+
+    return { size, rest };
+  },
+};
+</script>
