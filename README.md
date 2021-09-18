9d643519bad9f70912d0f8b71daaf9cb23b62ed7
张明华 <1416900346@qq.com>
2021-09-18 23:44:14 +0800

    项目初始化和Switch组件完成

---
+++ README.md
@@ -0,0 +1,1 @@
+# vue3-yun-ui
---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Switch v-model:value="bool"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+  setup() {
+    const bool = ref(false);
+    return { bool };
+  },
+};
+</script>
