3ef9fc7dce969fe02447d0c343e97ed97481c00f
张明华 <1416900346@qq.com>
2021-09-22 00:01:15 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,40 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="fn1"
+    :cancel="fn2"
+  ></Dialog>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Dialog from "../lib/Dialog.vue";
 export default {
   components: {
     Dialog,
+  },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const fn1 = () => {
+      console.log(1);
+      return false;
+    };
+    const fn2 = () => {
+      console.log(2);
+    };
+    return {
+      x,
+      toggle,
+      fn1,
+      fn2,
+    };
   },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,139 @@
 <template>
-<div class="dialog-overlay"></div>
-<div class="dialog-wrapper">
-  <header>标题</header>
-  <main>
-    <p>第一行字</p>
-    <p>第二行字</p>
-  </main>
-  <footer>
-    <Button>OK</Button>
-    <Button>Cancel</Button>
-  </footer>
-</div>
+  <template v-if="visible">
+    <div class="gulu-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="gulu-dialog-wrapper">
+      <div class="gulu-dialog">
+        <header>
+          标题 <span class="gulu-dialog-close" @click="close"></span>
+        </header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script lang="ts">
-import Button from './Button.vue'
+import Button from "./Button.vue";
 export default {
   components: {
-    Button
+    Button,
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancel: {
+      type: Function,
+    },
+  },
+  setup(props, { emit }) {
+    const close = () => {
+      emit("update:visible", false);
+    };
+
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancel = () => {
+      emit("cancel");
+      close();
+    };
+
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+.gulu-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  >header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  >main {
+    padding: 12px 16px;
+  }
+  >footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
   }
 }
-</script>
+</style>
