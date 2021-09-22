461bca4f478e3ae1f1f978e69afb959dfe8e8584
张明华 <1416900346@qq.com>
2021-09-22 11:23:32 +0800

    优化Tabs

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
   <h1>示例1</h1>
   <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
-    <Tab title="这是我的导航2">内容2</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -14,7 +14,7 @@
         @click="select(t)"
         :ref="
           (el) => {
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
       >
@@ -39,7 +39,7 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, ref, onUpdated } from "vue";
+import { computed, ref, watchEffect } from "vue";
 
 import Tab from "./Tab.vue";
 export default {
@@ -49,23 +49,18 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      const { width } = result.getBoundingClientRect();
+    watchEffect(() => {
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
-    };
-    onMounted(x);
-    onUpdated(x);
+    });
+
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -79,6 +74,7 @@ export default {
     //     return tag.props.title === props.selected;
     //   })[0];
     // });
+
     const current = computed(() => {
       return defaults.find((tag) => tag.props.title === props.selected);
     });
@@ -93,11 +89,11 @@ export default {
     return {
       defaults,
       titles,
-      current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
+      current
     };
   },
 };
