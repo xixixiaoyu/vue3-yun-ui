9662b098ae4eeea7555de98d12639910fa995e09
张明华 <1416900346@qq.com>
2021-09-22 17:27:32 +0800

    支持展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,51 +1,52 @@
 <template>
-<div>
-  <h1>Switch 组件示例 </h1>
-  <div class="demo">
-    <h2>常规用法</h2>
-    <div class="demo-component">
-      <Switch v-model:value="bool" />
+  <div>
+    <h1>Switch 组件示例</h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{ Switch1Demo.__sourceCode }}</pre>
+      </div>
     </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+    <div class="demo">
+      <h2>支持 disabled</h2>
+      <div class="demo-component">
+        <component :is="Switch2Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+      </div>
     </div>
   </div>
-  <div class="demo">
-    <h2>支持 disabled </h2>
-    <div class="demo-component">
-      <Switch v-model:value="bool" disabled />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-      <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
-    </div>
-  </div>
-</div>
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue'
-import {
-  ref
-} from 'vue'
+import Switch from "../lib/Switch.vue";
+import Button from "../lib/Button.vue";
+import Switch1Demo from "./Switch1.demo.vue";
+import Switch2Demo from "./Switch2.demo.vue";
+import { ref } from "vue";
 export default {
   components: {
-    Switch,
-    Button
+    Button,
   },
   setup() {
-    const bool = ref(false)
+    const bool = ref(false);
     return {
-      bool
-    }
-  }
-}
+      bool,
+      Switch1Demo,
+      Switch2Demo,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -53,7 +54,7 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-  >h2 {
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
@@ -68,9 +69,9 @@ $border-color: #d9d9d9;
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    >pre {
+    > pre {
       line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
+      font-family: Consolas, "Courier New", Courier, monospace;
       margin: 0;
     }
   }
