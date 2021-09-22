385c94769611854f30354111cc55fe0d5308f955
张明华 <1416900346@qq.com>
2021-09-22 09:49:17 +0800

    Tabs检查子组件的类型

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Tabs 组件</div>
+</template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+    });
+  },
+};
+</script>
