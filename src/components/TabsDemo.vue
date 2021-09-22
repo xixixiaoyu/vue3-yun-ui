e8cc6864c0d743191f0a880e8a6d183b8f5ea2c4
张明华 <1416900346@qq.com>
2021-09-22 11:15:33 +0800

    Tabs完成选中切换

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,26 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
+    <Tab title="这是我的导航2">内容2</Tab>
   </Tabs>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
 export default {
   components: {
     Tabs,
     Tab,
+  },
+  setup() {
+    const x = ref("导航1");
+    return {
+      x,
+    };
   },
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,14 +2,70 @@
   <div>Tabs 组件</div>
   <!-- <component :is="defaults[0]"></component>
   <component :is="defaults[1]"></component> -->
-  <div v-for="(t, index) in title" :key="index">{{ t }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
+  <!-- <div v-for="(t, index) in title" :key="index">{{ t }}</div>
+  <component v-for="(c, index) in defaults" :is="c" :key="index"></component> -->
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref="container">
+      <div
+        class="gulu-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+        :class="{ selected: t === selected }"
+        @click="select(t)"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
+      >
+        {{ t }}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <!-- <component
+        class="gulu-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      /> -->
+      <component
+        class="gulu-tabs-content-item"
+        :is="current"
+        :key="current.props.title"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import { computed, onMounted, ref, onUpdated } from "vue";
+
 import Tab from "./Tab.vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0];
+      const { width } = result.getBoundingClientRect();
+      indicator.value.style.width = width + "px";
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -17,13 +73,69 @@ export default {
       }
     });
 
-    const title = defaults.map((tag) => {
+    // const current = computed(() => {
+    //   console.log("重新 return");
+    //   return defaults.filter((tag) => {
+    //     return tag.props.title === props.selected;
+    //   })[0];
+    // });
+    const current = computed(() => {
+      return defaults.find((tag) => tag.props.title === props.selected);
+    });
+
+    const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
     return {
       defaults,
-      title,
+      titles,
+      current,
+      select,
+      navItems,
+      indicator,
+      container,
     };
   },
 };
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
