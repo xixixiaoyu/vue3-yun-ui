75af31f8cfe9aec9cb2ec91ea0296613f02fd7b1
张明华 <1416900346@qq.com>
2021-09-29 23:57:36 +0800

    增加文档attr

--- src/components/DialogDemo/DialogDemo.vue
+++ src/components/DialogDemo/DialogDemo.vue
@@ -2,20 +2,48 @@
   <div>Dialog 示例</div>
   <Demo :component="Dialog1Demo" title="基本用法" description="弹出一个对话框"/>
   <Demo :component="Dialog2Demo" title="函数式调用" description="隐藏dom结构，只暴露出调用的api"/>
+  <attr :columns="columns" :data="data"></attr>
 </template>
 
 <script lang="ts">
 import Demo from "../Demo.vue";
 import Dialog1Demo from "./Dialog1.demo.vue";
 import Dialog2Demo from "./Dialog2.demo.vue";
+import { columns } from "../../lib/data";
+import Attr from "../Attr.vue";
 export default {
   components: {
-    Demo,
+    Demo,Attr
   },
   setup() {
+     const data = [
+      {
+        params: 'visible',
+        desc: '是否展示',
+        type: 'boolean',
+        select: 'true / false',
+        default: 'normal',
+      },
+      {
+        params: 'ok',
+        desc: '确认触发函数',
+        type: 'function',
+        select: '()=>{}',
+        default: '--',
+      },
+      {
+        params: 'cancel',
+        desc: '取消触发函数',
+        type: 'function',
+        select: '()=>{}',
+        default: '--',
+      },
+    ];
     return {
       Dialog1Demo,
       Dialog2Demo,
+      columns,
+      data
     };
   },
 };
--- src/components/TabsDemo/TabsDemo.vue
+++ src/components/TabsDemo/TabsDemo.vue
@@ -1,18 +1,32 @@
 <template>
   <div>Tabs 示例</div>
-  <Demo :component="Tabs1Demo" title="tab切换" description="默认选中第一项。"/>
+  <Demo :component="Tabs1Demo" title="tab切换" description="默认选中第一项。" />
+  <attr :columns="columns" :data="data"></attr>
 </template>
 
 <script lang="ts">
 import Demo from "../Demo.vue";
 import Tabs1Demo from "./Tabs1.demo.vue";
+import { columns } from "../../lib/data";
+import Attr from "../Attr.vue";
 export default {
   components: {
-    Demo,
+    Demo, Attr
   },
   setup() {
+    const data = [
+      {
+        params: "Tab",
+        desc: "tab页签",
+        type: "component",
+        select: "Tab",
+        default: "--",
+      },
+    ];
     return {
       Tabs1Demo,
+      columns,
+      data,
     };
   },
 };
